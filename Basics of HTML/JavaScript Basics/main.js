f9f2750e8161cf6dbc0d672697fcabd127683a46
Hirendra77 <119104906+Hirendra77@users.noreply.github.com>
2022-12-29 12:54:15 +0530

    initial comit

---
+++ Basics of HTML/JavaScript Basics/main.js
@@ -0,0 +1,25 @@
+
+console.log("Hello World!")
+
+var suman;
+console.log(typeof suman);
+
+// Alert
+alert("hello")
+
+// prompt
+// prompt("title",[default])
+var age=prompt("enter age")
+if(age>18){
+    alert("eligible")
+}
+else{
+    alert("not eligible")
+}
+
+// confirm
+var isTestGiven=confirm("Did you complete the test")
+console.log( isTestGiven);
+
+var name=prompt("enter your name")
+alert("name is:"+ name)
